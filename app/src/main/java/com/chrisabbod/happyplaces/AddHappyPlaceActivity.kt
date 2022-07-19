a24b7bb09103897b8449b7f7b8cf48b3606a5fef
chris <chrisabbod@gmail.com>
2022-07-19 12:02:24 -0400

    Add date set functionality to date edit text

--- app/src/main/java/com/chrisabbod/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/chrisabbod/happyplaces/AddHappyPlaceActivity.kt
@@ -1,12 +1,20 @@
 package com.chrisabbod.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.chrisabbod.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddHappyPlaceActivity : AppCompatActivity() {
 
     private var binding: ActivityAddHappyPlaceBinding? = null
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +26,30 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        //OnDateSetListener: used to indicate when the user has finished selecting a date
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        binding?.etDate?.setOnClickListener {
+            DatePickerDialog(
+                this@AddHappyPlaceActivity,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "MM.dd.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
     }
 }
