ac5a63f51f132612182619bf5ac731f5077d99f8
chris <chrisabbod@gmail.com>
2022-07-19 22:17:52 -0400

    Use Dexter library to ask for user permissions

--- app/src/main/java/com/chrisabbod/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/chrisabbod/happyplaces/AddHappyPlaceActivity.kt
@@ -1,13 +1,24 @@
 package com.chrisabbod.happyplaces
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.Settings
+import android.widget.Toast
 import com.chrisabbod.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
+import java.util.jar.Manifest
 
 class AddHappyPlaceActivity : AppCompatActivity() {
 
@@ -44,6 +55,75 @@ class AddHappyPlaceActivity : AppCompatActivity() {
                 cal.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
+        binding?.tvAddImage?.setOnClickListener {
+            val pictureDialog = AlertDialog.Builder(this)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItems = arrayOf(
+                "Select photo from Gallery",
+                "Capture photo from camera"
+            )
+            pictureDialog.setItems(pictureDialogItems) { dialog, which ->
+                when (which) {
+                    0 -> choosePhotoFromGallery()
+                    1 -> {
+                        Toast.makeText(
+                            this@AddHappyPlaceActivity,
+                            "Camera selection coming soon",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }.show()
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this@AddHappyPlaceActivity).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Storage READ/WRITE permissions granted. " +
+                                "Now you can select an image from the gallery",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>,
+                token: PermissionToken
+            ) {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this@AddHappyPlaceActivity)
+            .setMessage(
+                "It looks like you have turned off permissions " +
+                        "required for this feature. It can be enabled under " +
+                        "application settings"
+            )
+            .setPositiveButton("GO TO SETTINGS")
+            { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView() {
